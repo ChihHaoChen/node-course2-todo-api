e5b58e42881792a6a4d3351e1988e20f6b3fb0f1
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-01-21 17:29:46 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+
+const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -81,6 +83,31 @@ app.delete('/todos/:id', (req, res) => {
       res.status(400).send();
     });
   }
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send('The data with this ID is not available.');
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 
