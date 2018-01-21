e8c864b7761f15262bae9d78c71f4c8e700c6a61
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-01-21 12:49:40 -0500

    Setup up the App for Heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ let { User } = require('./models/user');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -61,8 +63,8 @@ app.get('/todos/:id', (req, res) => {
 
 
 if(!module.parent) {
-  app.listen(3000, () => {
-    console.log(`Start on port 3000.`);
+  app.listen(port, () => {
+    console.log(`Started up at port ${port}.`);
   });
 }
 module.exports = { app }; //since the module we want to export also called app
