849fbdf361e5d80f397845130ec6e25c8dcbecde
ChihHaoChen <chih-hao.chen2@mail.mcgill.ca>
2018-01-25 14:34:18 -0500

    Add configuration for the deployment into GCP

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,8 @@ console.log(`env ***********${env}`);
 
 if (env === 'development') {
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  //process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+  process.env.MONGODB_URI = 'mongodb://chao0716:Monkland4410@ds111568.mlab.com:11568/todo_api';
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,3 +1,4 @@
+
 const expect = require('expect');
 const request = require('supertest');
 const { ObjectID } = require('mongodb');
@@ -5,21 +6,15 @@ const { ObjectID } = require('mongodb');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333
-}];
+const { todos,
+        populateTodos,
+        users,
+        populateUsers
+} = require('./seed/seed');
 
-beforeEach((done) => {
-  Todo.remove({}).then(() => {
-    return Todo.insertMany(todos);
-  }).then(() => done());
-});
+// beforeEach is used to initiate the test database.
+beforeEach(populateTodos);
+beforeEach(populateUsers);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
